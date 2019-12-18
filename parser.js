71170471e2d216584e4ae197266e90321e73ea9e
Zico Deng <zicodeng@gmail.com>
2019-12-18 12:14:22 -0800

    Change dataset and add chinese name

--- parser.js
+++ parser.js
@@ -7,31 +7,12 @@ console.log('Parsing CSV...');
 csv()
   .fromFile('./us-colleges.csv')
   .then(items => {
-    const colleges = [];
-
-    items.forEach(item => {
-      const name = item.institution.name;
-      const zip_code = item.HD2013.ZIP.code;
-      const state_name = item.HD2013.State.abbreviation;
-      const state_abbreviation = item.state.x;
-      const city = item.city.x;
-      const latitude = parseFloat(item.latitude);
-      const longitude = parseFloat(item.longitude);
-      const college = {
-        name,
-        zip_code,
-        state_name,
-        state_abbreviation,
-        city,
-        latitude,
-        longitude,
-      };
-
-      colleges.push(college);
-    });
-
+    const colleges = items.map(({ latitude, longitude, ...rest }) => ({
+      latitude: parseFloat(latitude),
+      longitude: parseFloat(longitude),
+      ...rest,
+    }));
     console.log(`Count: ${colleges.length}`);
-
     console.log('Writing parsed data to us-colleges.json');
     fs.writeFileSync('us-colleges.json', JSON.stringify(colleges), 'utf8');
   });
